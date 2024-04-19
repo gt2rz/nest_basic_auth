0ce1081cdc10e2b3e719055b7441761afec866cc
Miguel Gutierrez <gt2rz.dev@gmail.com>
2024-04-19 14:29:53 -0500

    chore: Remove password and confirm_password from registration response

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,6 +6,12 @@ export class AuthService {
   constructor() {}
 
   async register(authRegisterUserDto: AuthRegisterUserDto): Promise<any> {
+
+    // Remove password and confirm_password from the response
+    authRegisterUserDto.confirm_password &&
+      delete authRegisterUserDto.confirm_password;
+    authRegisterUserDto.password && delete authRegisterUserDto.password;
+
     return {
       message: 'User registered successfully',
       data: authRegisterUserDto,
