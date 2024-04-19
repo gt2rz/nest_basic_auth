66bfc01df0476e87a9747357455298b48eb49049
Miguel Gutierrez <gt2rz.dev@gmail.com>
2024-04-19 18:42:29 -0500

    chore: Add Twilio module to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma.service';
+import { TwilioModule } from 'nestjs-twilio';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TwilioModule.forRoot({
+      accountSid: process.env.TWILIO_ACCOUNT_SID,
+      authToken: process.env.TWILIO_AUTH_TOKEN,
+    }),
+    AuthModule,
+  ],
   controllers: [],
   providers: [PrismaService],
 })
