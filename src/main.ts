4c3de8fe7c11cdd9f6d7e193d4e52f8897474383
Miguel Gutierrez <gt2rz.dev@gmail.com>
2024-04-19 14:33:04 -0500

    chore: Update npm dependencies for class-transformer and class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove any extra properties sent by the client
+      forbidNonWhitelisted: true, // If any extra properties are sent, return an error
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
