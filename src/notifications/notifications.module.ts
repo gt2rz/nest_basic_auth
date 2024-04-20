691c3d7fcb7c7884b75c133949e42a647d4d1cde
Miguel Gutierrez <gt2rz.dev@gmail.com>
2024-04-19 20:50:51 -0500

    chore: Update npm dependencies for email and SMS notifications

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TwilioModule } from 'nestjs-twilio';
 import { NotificationsService } from './notifications.service';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
     TwilioModule.forRoot({
       accountSid: process.env.TWILIO_ACCOUNT_SID,
       authToken: process.env.TWILIO_AUTH_TOKEN,
+    }),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.EMAIL_HOST,
+        port: parseInt(process.env.EMAIL_PORT),
+        secure: process.env.EMAIL_SECURE == 'true',
+        // tls: {
+        //   ciphers: 'SSLv3',
+        // },
+        // requireTLS: true,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      },
     }),
   ],
   providers: [NotificationsService],
